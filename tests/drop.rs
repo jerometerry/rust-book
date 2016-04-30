cce6b539e47c0b2790859591a00b140cc044591b
Jerome Terry <jerome.l.terry@gmail.com>
2016-04-30 19:30:38 -0230

    4.20 Drop

---
+++ tests/drop.rs
@@ -0,0 +1,28 @@
+struct HasDrop;
+
+impl Drop for HasDrop {
+    fn drop(&mut self) {
+        println!("Dropping!");
+    }
+}
+
+#[test]
+fn test_drop() {
+    let x = HasDrop;
+} // drop will be called here, whex x goes out of scope
+
+struct Firework {
+    strength: i32,
+}
+
+impl Drop for Firework {
+    fn drop(&mut self) {
+        println!("BOOM times{}!!!", self.strength);
+    }
+}
+
+#[test]
+fn test_multi_drop() {
+    let firecracker = Firework { strenght: 1 };
+    let tnt = Firework { strength: 100 };
+} // tnt is dropped before firecracker. Drop is LIFO
